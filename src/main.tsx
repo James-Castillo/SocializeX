ca017b335bead020e79efb7ca1a671799b612603
James Castillo <159741683+James-Castillo@users.noreply.github.com>
2024-04-21 22:10:03 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "./App.tsx";
 import {AuthProvider} from "./context/AuthContext.tsx";
 import { QueryProvider } from "./lib/react-query/QueryProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <HashRouter>
     <QueryProvider>
       <AuthProvider>
         <App />
       </AuthProvider>
     </QueryProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
